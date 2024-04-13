1f1a0d9ec4bf4516048b99873d269133142a4a8c
tn-marcos-pajon <marcos.pajon@truenorth.co>
2024-04-13 18:49:15 -0300

    Solved exercise 3. Still searching improvements on time execution

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,19 @@
 from typing import List, Tuple
+import ujson
+import json
+from jsonpath_ng import parse
+from collections import Counter
+
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # JSONPATH where are the mentioned user in the tweet
+    jsonpath_expr = parse('$[*].mentionedUsers[*].username')
+
+    def read_json():
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            return ujson.loads(f"[{','.join(lines)}]")
+
+    items = read_json()
+
+    return Counter([match.value for match in jsonpath_expr.find(items)]).most_common(10)
